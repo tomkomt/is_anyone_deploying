78cc2fc113a0949b3349d859454465d2dabeb8f9
Marek Tomko <MTomko@sk.ibm.com>
2017-09-22 15:36:32 +0200

    Some changes

--- is_anyone_deploying.ino
+++ is_anyone_deploying.ino
@@ -11,10 +11,9 @@ int redLedPin = 5;
 int yelLedPin = 0;
 int greLedPin = 2;                                                                                                                                                                                                      ;
 
-char* wifiSsid = "12te";
-char* wifiPswd = "tusomdoma";
+char* httpUrl = "http://is-toni-deploying.eu-gb.mybluemix.net/esp/opm/";
 char* host = "is-toni-deploying.eu-gb.mybluemix.net";
-char* url = "/esp/opm";
+char* url = "/esp/opm/";
 char* certThumbprint = "4B E9 5D 98 11 8E E1 65 B6 53 7E 21 FC 2B F7 BE 73 36 85 0F";
 
 void setup() {
@@ -23,21 +22,26 @@ void setup() {
   pinMode(greLedPin, OUTPUT);
   Serial.begin(115200);
 
-  WiFiMulti.addAP(wifiSsid, wifiPswd);
-
+  WiFiMulti.addAP("12te", "tusomdoma");
+  WiFiMulti.addAP("HONOR_PLK_D33F", "12345678");
+  WiFiMulti.addAP("ECP-4G", "whJef399csdF45HELL");
+  
   diodesTest();
 }
 
 void loop() {
   if ((WiFiMulti.run() == WL_CONNECTED)) {
     Serial.println("[WIFI] Connected");
+    Serial.println(WiFi.SSID());
+    Serial.println(WiFi.localIP());
 
     HTTPClient http;
     Serial.println("[HTTPS] begin...");
-    http.begin(host, 443, url, true, certThumbprint);
+//    http.begin(host, 443, url, true, certThumbprint);
+    http.begin(host, 80, url);
     Serial.println("[HTTPS] connect...");
-    int httpCode = http.GET();
     getPendingState();
+    int httpCode = http.GET();
     if (httpCode > 0) {
       Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
       String payload = http.getString();
@@ -65,11 +69,11 @@ void loop() {
       }
     } else {
       Serial.printf("[HTTPS] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      notConnectedState();
+      notHttpConnectedState();
     }
     http.end();
   } else {
-    Serial.println("[WIFI] Not connected");
+    Serial.println("[WIFI] Not connected to wifi");
     notConnectedState();
   }
   Serial.println("Reconnecting...");
@@ -92,6 +96,22 @@ void notConnectedState() {
     digitalWrite(yelLedPin, LOW);
     delay(600);
   }
+}
+
+void notHttpConnectedState() {
+  shutAll();
+  for(int i=0;i<25;i++) {
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(yelLedPin, HIGH);
+    delay(300);
+    digitalWrite(yelLedPin, LOW);
+    delay(150);
+    digitalWrite(yelLedPin, HIGH);
+    delay(150);
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(yelLedPin, LOW);
+    delay(600);
+  }  
 }
 
 void getMismatchState() {
